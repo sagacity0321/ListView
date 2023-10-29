0d6f532f2834bb4d2b2d6c4265c3a16de82a9dac
sagacity0321 <citronica0321@gmail.com>
2023-10-29 14:14:41 +0900

    경고창 띄우기: 삭제하시겠습니까?

--- app/src/main/java/com/example/listview/MainActivity.kt
+++ app/src/main/java/com/example/listview/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.listview
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
 import com.example.listview.adapters가.StudentAdapter
 import com.example.listview.databinding.ActivityMainBinding
@@ -30,20 +32,32 @@ class MainActivity : AppCompatActivity() {
         binding.studentListView.adapter = mStdAdapter
 
         // 학생 한 명 클릭 -> "이름: 연락처" 로 토스트 출력
-        binding.studentListView.setOnItemClickListener{ adapterView, view, position, l ->
+        binding.studentListView.setOnItemClickListener { adapterView, view, position, l ->
             // 함수의 세 번째 (position) 파라미터: 몇 번 줄이 눌렸는지 알려줌
             // mStudentList 중 클릭된 줄에 맞는 학생 데이터 추출
             val clickedStd = mStudentList[position]
-            Toast.makeText(this, "${clickedStd.name}: ${clickedStd.phoneNum}", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "${clickedStd.name}: ${clickedStd.phoneNum}", Toast.LENGTH_SHORT)
+                .show()
         }
 
-        // 학생 한 명 길게 클릭 -> 해당 학생 삭제
+        // 학생 한 명 길게 클릭 -> 정말 지울건지? OK -> 해당 학생 삭제
         binding.studentListView.setOnItemLongClickListener { adapterView, view, position, l ->
-            // 오래 클릭된 학생 -> 목록에서 삭제
-            mStudentList.removeAt(position)
+            val std = mStudentList[position]
+            // 경고창 띄워서 확인
+            val alert = AlertDialog.Builder(this)
+            alert.setTitle("삭제 확인")
+            alert.setMessage("정말 ${std.name}학생을 삭제하시겠습니까?")
+            // 삭제 기능은 확인 버튼이 눌릴 때 실행
+            alert.setPositiveButton("확인", DialogInterface.OnClickListener { dialogInterface, i ->
+                // 오래 클릭된 학생 -> 목록에서 삭제
+                mStudentList.removeAt(position)
 
-            // 어댑터에게 새로고침 시키기
-            mStdAdapter.notifyDataSetChanged()
+                // 어댑터에게 새로고침 시키기
+                mStdAdapter.notifyDataSetChanged()
+            })
+            // 취소 버튼이 눌리면 아무일도 안 일어나도록
+            alert.setNegativeButton("취소", null)
+            alert.show()
 
             // LongClick: return값이 Boolean
             // true: 롱클릭만 실행
