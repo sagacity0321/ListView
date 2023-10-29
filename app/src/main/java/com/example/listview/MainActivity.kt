458a21b7c0b15aa3ae7c8504a0d68a59851f338b
sagacity0321 <citronica0321@gmail.com>
2023-10-29 11:39:48 +0900

    어댑터 클래스에서 리스트 아이템 xml 끌어오는 작업

--- app/src/main/java/com/example/listview/MainActivity.kt
+++ app/src/main/java/com/example/listview/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.listview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.listview.datas.StudentData
 
 class MainActivity : AppCompatActivity() {
+    val mStudentList = ArrayList<StudentData>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
--- app/src/main/java/com/example/listview/adapters/StudentAdapter.kt
+++ app/src/main/java/com/example/listview/adapters/StudentAdapter.kt
@@ -1,7 +1,11 @@
 package com.example.listview.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.example.listview.R
 import com.example.listview.datas.StudentData
 
 class StudentAdapter(
@@ -9,4 +13,15 @@ class StudentAdapter(
     resId: Int,
     mList: ArrayList<StudentData>
 ): ArrayAdapter<StudentData>(mContext, resId, mList) { // ArrayAdapter: 기본 생성자 지원X
+    val inf = LayoutInflater.from(mContext) // LayoutInflater: XML -> 객체화
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView // 재활용성: convertView
+        if (tempRow == null) {
+            tempRow = inf.inflate(R.layout.student_list_item, null)
+        }
+        val row = tempRow!! // !!: null이 아님을 보장
+
+        return row
+    }
 }
