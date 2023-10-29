48812a5d18587b5a53146cac04c407a8b28ccfb4
sagacity0321 <citronica0321@gmail.com>
2023-10-29 13:16:37 +0900

    getView 에서 실제 데이터 연동 - row.findById 로 UI 요소 변수에 담기 - 데이터 세팅(로직 작성)

--- app/src/main/java/com/example/listview/adapters/StudentAdapter.kt
+++ app/src/main/java/com/example/listview/adapters/StudentAdapter.kt
@@ -5,13 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import com.example.listview.R
 import com.example.listview.datas.StudentData
 
 class StudentAdapter(
     mContext: Context,
     resId: Int,
-    mList: ArrayList<StudentData>
+    val mList: ArrayList<StudentData>
 ): ArrayAdapter<StudentData>(mContext, resId, mList) { // ArrayAdapter: 기본 생성자 지원X
     val inf = LayoutInflater.from(mContext) // LayoutInflater: XML -> 객체화
 
@@ -20,7 +21,20 @@ class StudentAdapter(
         if (tempRow == null) {
             tempRow = inf.inflate(R.layout.student_list_item, null)
         }
+        // xml을 그려낸 row 객체
         val row = tempRow!! // !!: null이 아님을 보장
+
+        val stdData = mList[position]// 학생객체 추출
+
+        // row 내부의 텍스트뷰들을 가져오기
+        // Adapter에서는 DataBinding 사용 불가 -> 직접 코드로 불러내야 함
+        val txtName = row.findViewById<TextView>(R.id.txtName)
+        val txtPhoneNum = row.findViewById<TextView>(R.id.txtPhoneNum)
+        val txtAge = row.findViewById<TextView>(R.id.txtAge)
+
+        txtName.text = stdData.name
+        txtPhoneNum.text = stdData.phoneNum
+        txtAge.text = "(${2023 - stdData.birthYear + 1}세)"
 
         return row
     }
--- app/src/main/java/com/example/listview/datas/StudentData.kt
+++ app/src/main/java/com/example/listview/datas/StudentData.kt
@@ -1,9 +1,9 @@
 package com.example.listview.datas
 
-class StudentData ( // StudentData 생성자 변경: 이름, 출생연도, 폰번 넣어서 생성하도록
-    name: String,
-    birthYear: Int,
-    phoneNum: String
-){
+class StudentData( // StudentData 생성자 변경: 이름, 출생연도, 폰번 넣어서 생성하도록
+    val name: String,
+    val birthYear: Int,
+    val phoneNum: String
+) {
 
 }
